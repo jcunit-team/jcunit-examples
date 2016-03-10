5790886476f53f0fa12253ff87671d5586ee7724
dakusui <dakusui@gmail.com>
2016-03-11 01:43:14 +0900

    Improve message on failure.

--- quadratic/src/test/java/com/github/dakusui/quadratic/session1/QuadraticEquationTest.java
+++ quadratic/src/test/java/com/github/dakusui/quadratic/session1/QuadraticEquationTest.java
@@ -11,16 +11,14 @@ import static org.junit.Assert.assertThat;
 
 /**
  * An example that tests QuadraticEquationSolver.
+ * <p/>
  * <ul>
- * <li>session 1: Initial version of QuadraticEquationSolverTest.</li>
+ * <li>session 1: Initial version of QuadraticEquationSolverTest.(runCount     = 53, failureCount = 39, ignoreCount  =  0)
+ * </li>
  * </ul>
  */
 @RunWith(JCUnit.class)
-public class QuadraticEquationTest1 {
-  public static final int runCount     = 53;
-  public static final int failureCount = 39;
-  public static final int ignoreCount  = 0;
-
+public class QuadraticEquationTest {
   @FactorField
   public int a;
   @FactorField
@@ -32,7 +30,9 @@ public class QuadraticEquationTest1 {
   public void solveEquation() {
     QuadraticEquation.Solutions s = new QuadraticEquation(a, b,
         c).solve();
-    assertThat(String.format("%dx1^2+%dx1+%d=0 {x1=%f}", a, b, c, s.x1), a * s.x1 * s.x1 + b * s.x1 + c, is(0.0));
-    assertThat(String.format("%dx2^2+%dx2+%d=0 {x2=%f}", a, b, c, s.x2), a * s.x2 * s.x2 + b * s.x2 + c, is(0.0));
+    double v1 = a * s.x1 * s.x1 + b * s.x1 + c, v2 = a * s.x2 * s.x2 + b * s.x2 + c;
+
+    assertThat(String.format("%d*x1^2+%d*x1+%d=%f {x1=%f}", a, b, c, v1, s.x1), v1, is(0.0));
+    assertThat(String.format("%d*x2^2+%d*x2+%d=%f {x2=%f}", a, b, c, v2, s.x2), v2, is(0.0));
   }
 }
