8c6a6b4470e2b446f82848c00faca9be2389c4a5
Hiroshi Ukai <dakusui@gmail.com>
2016-10-31 05:39:37 +0900

    Add reverse regex module and its test.

--- geophile/src/main/java/com/geophile/z/util/ReverseRegex.java
+++ geophile/src/main/java/com/geophile/z/util/ReverseRegex.java
@@ -1,7 +1,38 @@
-package com.geophile.z;
+package com.geophile.z.util;
 
-/**
- * Created by hiroshi on 10/31/16.
- */
-public class ReverseRegex {
+import com.github.dakusui.jcunit.plugins.levelsproviders.LevelsProvider;
+import nl.flotsam.xeger.Xeger;
+
+import java.util.ArrayList;
+import java.util.Random;
+
+import static com.google.common.base.Preconditions.checkArgument;
+import static com.google.common.base.Preconditions.checkNotNull;
+
+public class ReverseRegex implements LevelsProvider {
+  private final ArrayList<String> levels;
+
+  public ReverseRegex(
+      @Param(source = Param.Source.CONFIG) String regex,
+      @Param(source = Param.Source.CONFIG, defaultValue = { "10" }) int numLevels,
+      @Param(source = Param.Source.CONFIG, defaultValue = { "1" }) long randomSeed
+  ) {
+    checkNotNull(regex);
+    checkArgument(numLevels > 0);
+    this.levels = new ArrayList<>(numLevels);
+    Xeger xeger = new Xeger(regex, new Random(randomSeed));
+    for (int i = 0; i < numLevels; i++) {
+      this.levels.add(xeger.generate());
+    }
+  }
+
+  @Override
+  public int size() {
+    return this.levels.size();
+  }
+
+  @Override
+  public String get(int n) {
+    return this.levels.get(n);
+  }
 }
--- geophile/src/test/java/com/github/dakusui/geophile/jcunit/ut/ReverseRegexProviderTest.java
+++ geophile/src/test/java/com/github/dakusui/geophile/jcunit/ut/ReverseRegexProviderTest.java
@@ -1,7 +1,44 @@
 package com.github.dakusui.geophile.jcunit.ut;
 
-/**
- * Created by hiroshi on 10/31/16.
- */
+import com.geophile.z.util.ReverseRegex;
+import com.github.dakusui.geophile.jcunit.TestUtils;
+import com.github.dakusui.jcunit.runners.standard.JCUnit;
+import com.github.dakusui.jcunit.runners.standard.TestCaseUtils;
+import com.github.dakusui.jcunit.runners.standard.annotations.FactorField;
+import com.github.dakusui.jcunit.runners.standard.annotations.Value;
+import org.junit.Test;
+import org.junit.runner.JUnitCore;
+import org.junit.runner.Result;
+import org.junit.runner.RunWith;
+
+import static org.junit.Assert.assertEquals;
+
 public class ReverseRegexProviderTest {
+
+  @RunWith(JCUnit.class)
+  public static class TestClass {
+    TestUtils.Out out = new TestUtils.Out();
+    @FactorField(
+        levelsProvider = ReverseRegex.class,
+        args = { @Value({ "(hello|Hello)world(!)?, (e|E)veryone\\. How are you( doing)?\\?" }) }
+    )
+    public String factor;
+
+    @FactorField
+    public int i;
+    @FactorField
+    public int j;
+
+    @Test
+    public void test() {
+      out.writeLine(TestCaseUtils.toTestCase(this));
+    }
+  }
+
+  @Test
+  public void testReverseRegexProvider() {
+    Result result = JUnitCore.runClasses(TestClass.class);
+    assertEquals(70, result.getRunCount());
+    assertEquals(0, result.getFailureCount());
+  }
 }
